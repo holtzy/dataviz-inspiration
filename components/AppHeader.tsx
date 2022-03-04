fd2911ee851040a072b89d75839e14bb0baac29a
holtzy <yan.holtz@datadoghq.com>
2022-03-04 16:23:59 +0100

    more content in header

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -20,7 +20,26 @@ export const AppHeader = ({
     <Head>
       <title>Dataviz Inspiration</title>
       <meta name="description" content={siteDescription} />
-      <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="icon"
+        href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💡</text></svg>"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Roboto+Mono:wght@300&display=swap"
+        rel="stylesheet"
+      />
+      <meta
+        name="keywords"
+        content="Viz, dataviz, data visualization, example, catalog, list, chart, graph, plot"
+      />
+      <meta name="author" content="Yan Holtz" />
+
+      <meta property="og:title" content={"Dataviz Inspiration"} />
+      <meta property="og:description" content={siteDescription} />
+      <meta
+        property="og:image"
+        content="https://raw.githubusercontent.com/holtzy/dataviz-inspiration/main/public/overview.png"
+      />
     </Head>
   );
 };
