2eb2e941afdb4c48303b40b912698ee1acee0eac
Yan Holtz <yan.holtz.data@gmail.com>
2024-07-09 15:40:16 +0200

    create the pictureWall componetn

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,19 @@
 "use client";
 
-import { useState } from "react";
 import { AppHeader } from "../components/AppHeader";
 import Navbar from "../components/Navbar";
 import TitleAndDescription from "../components/TitleAndDescription";
-import { WallFilters } from "../components/WallFilters";
-import { MasonryItem } from "../components/MasonryItem";
-import { VizItemModal } from "../components/VizItemModal";
 import Footer from "../components/Footer";
-import { filterVizList } from "../util/filterVizList";
-import { Luminosity, Tool, VizItem, vizList } from "../util/viz-list";
-import { useSearchParams } from "next/navigation";
-import { Masonry } from "masonic";
-import useLikesData from "../hooks/use-likes";
-
-export type Project = { projectId: number; imgId: number };
-
-type MasonryCardProps = {
-  index: number;
-  data: VizItem;
-  width: number;
-};
+import { vizList } from "../util/viz-list";
+import { PictureWall } from "../components/PictureWall";
 
 export default function Page() {
-  const searchParams = useSearchParams();
-  const columnNumber = searchParams.get("columnNumber") || "3";
-  const luminosity = (searchParams.get("luminosity") || undefined) as
-    | Luminosity
-    | undefined;
-  const tools = (searchParams.getAll("tools") || undefined) as
-    | Tool[]
-    | undefined;
-
-  // State of this specific page
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const likesData = useLikesData();
-
-  // specify the project (id in the viz-list.ts array) + the img id (some projects have several imgs)
-  const [selectedProject, setSelectedProject] = useState<Project | undefined>();
-
-  // Apply the filters on the viz list!
-  const filteredVizList = filterVizList(vizList, luminosity, undefined, tools);
-
-  const vizItemNumber = filteredVizList.length;
-
   const siteDescription = (
     <p>
       <a href="https://www.dataviz-inspiration.com">Dataviz-inspiration.com</a>
       <span>
         {" aims at being the biggest list of chart examples available on the web. It showcases " +
-          vizItemNumber +
+          vizList.length +
           " of the most beautiful and impactful dataviz projects I know. The collection is a good place to visit when you're designing a new graph, together with "}
       </span>
       <a href="https://www.data-to-viz.com">data-to-viz.com</a>
@@ -58,27 +21,13 @@ export default function Page() {
     </p>
   );
 
-  const MasonryCard = ({ index, data, width }: MasonryCardProps) => {
-    return (
-      <MasonryItem
-        vizItem={data}
-        onClick={() => {
-          setSelectedProject({ projectId: data.id, imgId: 0 });
-          setIsModalOpen(true);
-        }}
-        imgId={0}
-        likeNumber={likesData[data.id] || 0}
-      />
-    );
-  };
-
   return (
     <>
       <AppHeader
         title={"Dataviz Inspiration | Hundreds of chart examples"}
         description={
           "The biggest list of chart examples available on the web. " +
-          vizItemNumber +
+          vizList.length +
           " of the best viz projects displayed using a pinterest style sorted by tool and chart type."
         }
       />
@@ -92,46 +41,8 @@ export default function Page() {
           title="Dataviz Inspiration"
           description={siteDescription}
         />
-
-        <WallFilters
-          columnNumber={Number(columnNumber)}
-          luminosity={luminosity}
-          selectedChart={undefined}
-          tools={tools}
-        />
-
-        <div
-          style={{
-            backgroundColor: "rgb(245, 245, 245)",
-          }}
-          className="w-full flex justify-center py-5"
-        >
-          <div
-            className="w-full px-4"
-            style={{ maxWidth: 1100, marginTop: 25 }}
-          >
-            <div className="w-full">
-              <Masonry
-                items={filteredVizList}
-                render={MasonryCard}
-                columnCount={Number(columnNumber)}
-                columnGutter={12}
-                rowGutter={12}
-              />
-            </div>
-          </div>
-        </div>
+        <PictureWall />
       </main>
-
-      {selectedProject && (
-        <VizItemModal
-          isModalOpen={isModalOpen}
-          selectedProject={selectedProject}
-          setSelectedProject={setSelectedProject}
-          closeModal={() => setIsModalOpen(false)}
-          likeNumber={likesData[selectedProject.projectId] || 0}
-        />
-      )}
 
       <div className="wrapper">
         <Footer />
---
+++ components/PictureWall.tsx
@@ -0,0 +1,94 @@
+import { Masonry } from "masonic";
+import { WallFilters } from "./WallFilters";
+import { VizItemModal } from "./VizItemModal";
+import { useSearchParams } from "next/navigation";
+import { MasonryItem } from "./MasonryItem";
+import { useState } from "react";
+import useLikesData from "../hooks/use-likes";
+import { Luminosity, Tool, VizItem, vizList } from "../util/viz-list";
+import { filterVizList } from "../util/filterVizList";
+
+export type Project = { projectId: number; imgId: number };
+
+type MasonryCardProps = {
+  index: number;
+  data: VizItem;
+  width: number;
+};
+
+export const PictureWall = () => {
+  //
+  // Most of the state is stored in the URL:
+  //
+  const searchParams = useSearchParams();
+  const columnNumber = searchParams.get("columnNumber") || "3";
+  const luminosity = (searchParams.get("luminosity") || undefined) as
+    | Luminosity
+    | undefined;
+  const tools = (searchParams.getAll("tools") || undefined) as
+    | Tool[]
+    | undefined;
+
+  //
+  // Additional state that is NOT in the URL
+  //
+  const [selectedProject, setSelectedProject] = useState<Project | undefined>(); // specify the project (id in the viz-list.ts array) + the img id (some projects have several imgs)
+  const [isModalOpen, setIsModalOpen] = useState(false); // State of this specific page
+
+  const likesData = useLikesData();
+  const filteredVizList = filterVizList(vizList, luminosity, undefined, tools);
+
+  const MasonryCard = ({ index, data, width }: MasonryCardProps) => {
+    return (
+      <MasonryItem
+        vizItem={data}
+        onClick={() => {
+          setSelectedProject({ projectId: data.id, imgId: 0 });
+          setIsModalOpen(true);
+        }}
+        imgId={0}
+        likeNumber={likesData[data.id] || 0}
+      />
+    );
+  };
+
+  return (
+    <>
+      <WallFilters
+        columnNumber={Number(columnNumber)}
+        luminosity={luminosity}
+        selectedChart={undefined}
+        tools={tools}
+      />
+
+      <div
+        style={{
+          backgroundColor: "rgb(245, 245, 245)",
+        }}
+        className="w-full flex justify-center py-5"
+      >
+        <div className="w-full px-4" style={{ maxWidth: 1100, marginTop: 25 }}>
+          <div className="w-full">
+            <Masonry
+              items={filteredVizList}
+              render={MasonryCard}
+              columnCount={Number(columnNumber)}
+              columnGutter={12}
+              rowGutter={12}
+            />
+          </div>
+        </div>
+      </div>
+
+      {selectedProject && (
+        <VizItemModal
+          isModalOpen={isModalOpen}
+          selectedProject={selectedProject}
+          setSelectedProject={setSelectedProject}
+          closeModal={() => setIsModalOpen(false)}
+          likeNumber={likesData[selectedProject.projectId] || 0}
+        />
+      )}
+    </>
+  );
+};
